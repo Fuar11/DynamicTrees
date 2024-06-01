a4afbba7c6dcc788b96b262a3107d72f9d9596fc
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2024-06-01 20:04:12 -0300

    Create DynamicTreeTexture.cs

---
+++ DynamicTreesComponent/DynamicTreeTexture.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicTrees.DynamicTreesComponent
+{
+	internal class DynamicTreeTexture
+	{
+		public string? Name { get; set; }
+		public Texture? Texture { get; set; }
+	}
+}

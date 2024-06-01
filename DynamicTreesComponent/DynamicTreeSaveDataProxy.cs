7db6c4ae5b3c72e662380351018034226d3e2213
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2024-06-01 20:04:06 -0300

    Update DynamicTreeSaveDataProxy.cs

--- DynamicTreesComponent/DynamicTreeSaveDataProxy.cs
+++ DynamicTreesComponent/DynamicTreeSaveDataProxy.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DynamicTrees.DynamicTreesComponent
 {
-    internal class DynamicTreeSaveDataProxy
+    public class DynamicTreeSaveDataProxy
     {
         public float savedAccumulation { get; set; }
     }
